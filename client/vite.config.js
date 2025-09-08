bfb1e4937955f80f96fa3194545f0e7ec05b4c74
Gourav <begouravdev@gmail.com>
2025-09-08 12:29:32 +0530

    code reverted and added whitelist ip

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  }
-});
+    port: 5173,
+    host: true, // listen on all addresses
+    allowedHosts: ["webrtc.turka.tech"], // 👈 add this
+  },
+})
